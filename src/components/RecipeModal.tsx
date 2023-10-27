06166d082e4a3349f22891334b9b6bf385ec4c30
Mackenzie Lamour <maclamour@gmail.com>
2023-10-27 09:44:30 -0400

    updated modal and logic to display summary info

--- src/components/RecipeModal.tsx
+++ src/components/RecipeModal.tsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react";
+import { RecipeSummary } from "../types";
+
 const RecipeModal = () => {
+
+  const [recipeSummary, setRecipeSummary] = useState<RecipeSummary>();
+
+  // useEffect(())
+
+  if (!recipeSummary){
+    return <></>
+  }
+
   return (
     <>
       <div className="overlay"></div>
       <div className="modal">
         <div className="modal-content">
           <div className="modal-header">
-            <h2>RECIPE TITLE</h2>
+            <h2>{recipeSummary?.title}</h2>
             <span className="close-btn">&times;</span>
           </div>
-          <p>RECIPE SUMMARY</p>
+          <p dangerouslySetInnerHTML={{__html: recipeSummary?.summary}}></p>
         </div>
       </div>
     </>
