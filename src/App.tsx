b05808990b9ad9277eee4ed0596a2a1d3f907ad7
Mackenzie Lamour <maclamour@gmail.com>
2023-10-27 07:32:13 -0400

    added input to UI so a user can enter in a search term on the form, updated state to handle this request

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import * as API from './API';
 import { Recipe } from "./types";
 
 const App = () => {
-  const [searchTerm, setsSearchTerm] = useState("burger");
+  const [searchTerm, setsSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([])
 
   const handleSearchSubmit =async (event: FormEvent) => {
@@ -24,6 +24,13 @@ const App = () => {
   return(
     <div>
       <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <input 
+        type='text' 
+        required
+        placeholder="Enter a search term..."
+        value={searchTerm}
+        onChange={(event)=> setsSearchTerm(event.target.value)}  
+        ></input>
         <button type='submit'>Submit</button>
       </form>
       {recipes.map((recipe) =>(
