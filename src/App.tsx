21fdf7e65ccf5394ddc2546cd438db1d0de837ed
Mackenzie Lamour <maclamour@gmail.com>
2023-10-27 07:19:07 -0400

    frontend was successfully able to connect to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 // import React from "react";
 
 import "./App.css"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 import * as API from './API';
 import { Recipe } from "./types";
 
 const App = () => {
-  const [searchTerm, setsSearchTerm] = useState("burgers");
+  const [searchTerm, setsSearchTerm] = useState("pasta");
   const [recipes, setRecipes] = useState<Recipe[]>([])
 
-  const handleSearchSubmit =async () => {
+  const handleSearchSubmit =async (event: FormEvent) => {
+    event.preventDefault()
     try {
       const recipes = await API.searchRecipes(searchTerm,1);
       setRecipes(recipes);
@@ -22,7 +23,7 @@ const App = () => {
 
   return(
     <div>
-      <form onSubmit={() => handleSearchSubmit()}>
+      <form onSubmit={(event) => handleSearchSubmit(event)}>
         <button type='submit'>Submit</button>
       </form>
       {recipes.map((recipe) =>(
