64c3e64751ede12739053a0879042b44d0c7ce3e
Mackenzie Lamour <maclamour@gmail.com>
2023-10-31 15:32:06 -0400

    updated code to connect client side code to with deployed server side code

--- src/API.ts
+++ src/API.ts
@@ -1,9 +1,8 @@
-// import { searchRecipes, getRecipeSummary, getFavoriteRecipesByIds } from '../../backend/src/recipeAPI';
 
 import { Recipe } from './types';
 
 const searchRecipes = async (searchTerm: string, page: number) => {
-  const baseUrl = new URL("http://localhost:5500/api/recipes/search");
+  const baseUrl = new URL("https://my-recipe-app-hmew.onrender.com/api/recipes/search");
   baseUrl.searchParams.append("searchTerm", searchTerm);
   baseUrl.searchParams.append("page", page.toString());
 
@@ -17,7 +16,7 @@ const searchRecipes = async (searchTerm: string, page: number) => {
 };
 
 const getRecipeSummary = async (recipeId: string) => {
-  const url = new URL(`http://localhost:5500/api/recipes/${recipeId}/summary`);
+  const url = new URL(`https://my-recipe-app-hmew.onrender.com/api/recipes/${recipeId}/summary`);
   const response = await fetch(url);
 
   if (!response.ok) {
@@ -27,7 +26,7 @@ const getRecipeSummary = async (recipeId: string) => {
 };
 
 const getFavoriteRecipes = async () => {
-  const url = new URL("http://localhost:5500/api/recipes/favorite");
+  const url = new URL("https://my-recipe-app-hmew.onrender.com/api/recipes/favorite");
   const response = await fetch(url);
 
   if (!response.ok) {
@@ -38,7 +37,7 @@ const getFavoriteRecipes = async () => {
 };
 
 const addFavoriteRecipe = async (recipe: Recipe) => {
-  const url = new URL("http://localhost:5500/api/recipes/favorite");
+  const url = new URL("https://my-recipe-app-hmew.onrender.com/api/recipes/favorite");
   const body = {
     recipeId: recipe.id,
   };
@@ -56,7 +55,7 @@ const addFavoriteRecipe = async (recipe: Recipe) => {
 };
 
 const removeFavoriteRecipe =async (recipe:Recipe) => {
-  const url = new URL("http://localhost:5500/api/recipes/favorite");
+  const url = new URL("https://my-recipe-app-hmew.onrender.com/api/recipes/favorite");
   const body = {
     recipeId: recipe.id,
   };
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import React from "react";
 
 import "./App.css";
 import { FormEvent, useEffect, useRef, useState } from "react";
